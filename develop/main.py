70424157d30f26227b35c32d70e253187f6da905
whitenesss <144126783+whitenesss@users.noreply.github.com>
2024-02-15 20:43:11 +0300

    fix main

--- develop/main.py
+++ develop/main.py
@@ -4,27 +4,6 @@ from develop.class_product import Category, Product
 from develop.import_json import file_json
 
 
-# def main():
-#     producti_json = file_json()
-#     list_category = []
-#     list_product = []
-#     for categories in producti_json:
-#         product_catalog = [proh for proh in categories["products"]]
-#         category = Category(categories['name'], categories['description'], categories['products'])
-#         list_category.append(category.request_name())
-#         list_category.append(category.request_description())
-#         list_category.append(category.request_products())
-#         for categories_1 in product_catalog:
-#             product = Product(categories_1['name'], categories_1['description'], categories_1['price'],
-#                               categories_1['quantity'])
-#             list_product.append(product.request_name())
-#             list_product.append(product.request_description())
-#             list_product.append(product.request_price())
-#             list_product.append(product.request_quantity())
-#     print(*list_category, sep='\n')
-#     print()
-#     print(list_product, sep='\n')
-
 def main():
     list_product_smart = []
     list_product_tv = []
@@ -36,8 +15,6 @@ def main():
                 list_product_smart.append(categories_1['name'])
             elif categories['name'] == 'Телевизоры':
                 list_product_tv.append(categories_1['name'])
-    # print(list_product_tv)
-    # print(list_product_smart)
 
     list_category = []
 
@@ -53,8 +30,6 @@ def main():
             list_category.append(category.request_description())
             list_category.append(category.request_products())
     print(*list_category, sep='\n')
-
-
 
 
 if __name__ == '__main__':
